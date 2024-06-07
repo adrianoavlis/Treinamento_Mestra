2b3ea89b77019db15f42a4142330f430f64c1d32
adrianoavlis <luisadriano@id.uff.br>
2024-06-07 17:05:51 -0300

    '07/06/2014

--- Respostas/Questões Bruna/2 - Querys/Atividade 6.sql
+++ Respostas/Questões Bruna/2 - Querys/Atividade 6.sql
@@ -1,4 +1,5 @@
 
+
 -- Atividades 19/03/2014   
 
 -- c) Listar os participantes que possuem todas as situações, desconsiderando a situação
@@ -6,36 +7,107 @@
 -- Código Patrocinadora, Matricula e Nome do Participante, Situação. 
 
 SELECT 
-	[IDPLA] as idParticipante,
-	[IDPAT] as cdPatrocinadora,
-	[NMPAR] as nmParticipante,
-	[CDCAT] as categoriaSituacao,
-	[CDSITPLA] as cdSituacao
-FROM PAR_PARPLA ISC , PAR_PATROCINADORAS PAT, PAR_DADOS_ANT_RECAD PART
-WHERE	[CDSITPLA] = 23 
-	AND [STISC] = 2 
-	AND PAT.[IDPAT]= ISC.CDPAT
-	AND	PART.NRMAT = ISC.NRMAT 
-ORDER BY DCPAT, NMPAR
+		PAT.CDPAT as CodigoPatrocinadora,
+		PART.NRMAT as Matricula,
+		PART.NMPAR as NomeParticipante,
+		ISC.CDSITPLA as Situacao
+FROM	
+		PAR_PARPLA ISC
+JOIN	
+		PAR_PATROCINADORAS PAT ON PAT.IDPAT = ISC.CDPAT
+JOIN	
+		PAR_PARTICIPANTES PART ON PART.NRMAT = ISC.NRMAT
+WHERE 
+		ISC.CDSITPLA <> 'AUTOPATROCIO PARCIAL'
+ORDER BY 
+		PAT.CDPAT, PART.NMPAR;
 GO
+
 
 
 -- 1) Listar a quantidade de parcelas já pagas para cada contrato VG.
 -- Campos: Codigo Patrocinadora, Matricula e Nome do Participante, Nro Contrato,
 -- Qtd Prestacoes pagas (informação buscada em EPT_PARCELA, campo DTMESREFPCL) 
 
+SELECT 
+    PAT.CDPAT as CodigoPatrocinadora,
+    PART.NRMAT as Matricula,
+    PART.NMPAR as NomeParticipante,
+    REQ.NRCTT as NroContrato,
+    COUNT(PCL.DTMESREFPCL) as QtdPrestacoesPagas
+FROM 
+    EPT_PARCELAS PCL
+JOIN 
+    EPT_REQUERIMENTO REQ ON REQ.IDREQEPT = PCL.IDREQEPT
+JOIN 
+    PAR_PARTICIPANTES PART ON PART.IDPAR = REQ.IDPAR
+JOIN 
+    PAR_PATROCINADORAS PAT ON PAT.IDPAT = PART.IDPAT
+WHERE 
+    REQ.IDSITEPT ='VG'
+GROUP BY 
+    PAT.CDPAT, PART.NRMAT, PART.NMPAR, REQ.NRCTT;
+GO
 
-
--- 2) Listar a qtd de parcelas já pagas para o participante matricula 0050018563, contrato VG
+-- 2) Listar a qtd de parcelas já pagas para o participante matricula 0001001311, contrato VG
 -- Campos: Codigo Patrocinadora, Matricula e Nome do Participante, Nro Contrato,
 -- Qtd Prestacoes pagas (informação buscada em EPT_PARCELA, campo DTMESREFPCL)
 
+SELECT 
+    PAT.CDPAT as CodigoPatrocinadora,
+    PART.NRMAT as Matricula,
+    PART.NMPAR as NomeParticipante,
+    REQ.NRCTT as NroContrato,
+    COUNT(PCL.DTMESREFPCL) as QtdPrestacoesPagas
+FROM 
+    EPT_PARCELAS PCL
+JOIN 
+    EPT_REQUERIMENTO REQ ON REQ.IDREQEPT = PCL.IDREQEPT
+JOIN 
+    PAR_PARTICIPANTES PART ON PART.IDPAR = REQ.IDPAR
+JOIN 
+    PAR_PATROCINADORAS PAT ON PAT.IDPAT = PART.IDPAT
+WHERE 
+    REQ.IDSITEPT = 'VG'
+    AND PART.NRMAT = '0001001311'
+GROUP BY 
+    PAT.CDPAT, PART.NRMAT, PART.NMPAR, REQ.NRCTT;
+GO
 
 		
 -- 3) Listar as parcelas em aberto, cobradas boleto (IDFORCBA = 3) no mes/ref 07/2012 
 -- Campos: Codigo Patrocinadora, Matricula e Nome do Participante, Nro Contrato,
 -- Valor da Prestação
 
+SELECT 
+    PAT.CDPAT as CodigoPatrocinadora,
+    PART.NRMAT as Matricula,
+    PART.NMPAR as NomeParticipante,
+    REQ.NRCTT as NroContrato,
+    PCL.VRPCL as ValorPrestacao
+FROM 
+    EPT_PARCELAS PCL
+JOIN 
+    EPT_REQUERIMENTO REQ ON REQ.IDREQEPT = PCL.IDREQEPT
+JOIN 
+    PAR_PARTICIPANTES PART ON PART.IDPAR = REQ.IDPAR
+JOIN 
+    PAR_PATROCINADORAS PAT ON PAT.IDPAT = PART.IDPAT
+WHERE 
+	PCL.IDFORCBA = 3
+    AND PCL.DTMESREFPCL = '201403'
+    AND PCL.STPCLEPT = 'A' 
+ORDER BY 
+	PCL.DTMESREFPCL
+GO
+
+SELECT IDFORCBA FROM EPT_PARCELAS
+
+SELECT COLUMN_NAME, TABLE_NAME
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE COLUMN_NAME LIKE '%IDFORCBA%'
+ORDER BY TABLE_NAME
+GO
 
 
 -- 4) Listar as parcelas em aberto no mes/ref 07/2012, cobradas SIAPE (IDFORCBA = 6) 
@@ -43,7 +115,7 @@ GO
 -- Valor da Prestação
 
 
--- 5) Listar as parcelas integradas para o SIAPE (TABELA CTB_ARQSIAPE) no mes/ref 07/2012 
+-- 5) Listar as parcelas integradas para o SIAPE (TABELA CTB_ARQ	SIAPE) no mes/ref 07/2012 
 -- Campos: Codigo Patrocinadora, Matricula e Nome do Participante, Valor da Prestação (VRINF)
 -- Condições: TPARQ = 'E', CDRUBSIA = '32525', STLAN = 'I'
 
